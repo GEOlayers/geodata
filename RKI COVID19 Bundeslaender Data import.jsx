76f6f96f0ca9604bd10e277f397815c76b1c7dbf
GEOlayers <geolayers.ae@gmail.com>
2021-02-04 13:56:02 +0100

    RKI COVID Daten

---
+++ RKI COVID19 Bundeslaender Data import.jsx
@@ -0,0 +1,4 @@
+﻿//Dieses Script importiert einen aktuellen Datensatz des Robert-Koch-Instituts zu COVID19 Erkrankungen. Mehr infos zur Quelle gibt es hier: https://npgeo-corona-npgeo-de.hub.arcgis.com/
+//Quellenanganbe: "Bundesamt für Kartographie und Geodäsie / Robert Koch-Institut"
+
+geolayers3.addToBrowser("https://opendata.arcgis.com/datasets/ef4b445a53c1406892257fe63129a8ea_0.geojson", {namingProp:"LAN_ew_GEN", name:"RKI Bundesländer " + (new Date).toDateString()})
---
+++ RKI COVID19 Landkreise Data import.jsx
@@ -0,0 +1,4 @@
+﻿//Dieses Script importiert einen aktuellen Datensatz des Robert-Koch-Instituts zu COVID19 Erkrankungen. Mehr infos zur Quelle gibt es hier: https://npgeo-corona-npgeo-de.hub.arcgis.com/
+//Quellenanganbe: "Bundesamt für Kartographie und Geodäsie / Robert Koch-Institut"
+
+geolayers3.addToBrowser("https://opendata.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0.geojson", {namingProp:"GEN", name:"RKI Landkreise " + (new Date).toDateString()})
