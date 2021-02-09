6058e85f6f97807fdd373b12b0f45c49f4f2bcb5
GEOlayers <geolayers.ae@gmail.com>
2021-02-09 19:42:23 +0100

    Create Historical Basemaps Browser.jsx

---
+++ Historical Basemaps Browser.jsx
@@ -0,0 +1,8 @@
+//Browse and import historical basemaps from https://github.com/aourednik/historical-basemaps
+geolayers3.githubBrowser({
+    title: "Historical Basemaps",
+    defaultUser: "aourednik",
+    defaultRepo: "historical-basemaps",
+    fixedRepo: true,
+    flatten: true,
+})
