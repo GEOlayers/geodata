7fc7332b5ce0b040821f7c0dad4afa110baee84e
GEOlayers <geolayers.ae@gmail.com>
2021-03-04 12:04:47 +0100

    Update Charging Stations OSM.jsx

--- Charging Stations OSM.jsx
+++ Charging Stations OSM.jsx
@@ -1,4 +1,4 @@
-//imports charging stations inside the current view
+//Imports charging stations inside the current view. You should zoom to city level.
 geolayers3.addToBrowser({
         type:"overpass",
         query:'[out:json][timeout:25];(node["amenity"="charging_station"]({{bbox}}););out body;>;out skel qt;'
