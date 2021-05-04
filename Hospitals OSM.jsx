3a03311e62427ad81fde6aab2801df89dea59f30
GEOlayers <geolayers.ae@gmail.com>
2021-05-04 09:28:53 +0200

    Update Hospitals OSM.jsx

--- Hospitals OSM.jsx
+++ Hospitals OSM.jsx
@@ -1,5 +1,5 @@
 //Imports hospitals inside the current view. You should zoom to country level at least.
 geolayers3.addToBrowser({
         type:"overpass",
-        query:'[out:json][timeout:25];(node["amenity"="hospital"]({{bbox}});relation["amenity"="hospital"]({{bbox}}););out body;>;out skel qt;'
+        query:'[out:json][timeout:45];(node["amenity"="hospital"]({{bbox}});way["amenity"="hospital"]({{bbox}});relation["amenity"="hospital"]({{bbox}}););out body;>;out skel qt;'
 })
